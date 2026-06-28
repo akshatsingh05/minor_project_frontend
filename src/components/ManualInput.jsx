20fe2bfc3a9a24e15f370f2e78210c15ef9e4459
akshatsingh05 <akshatsatishsingh@gmail.com>
2026-06-28 13:33:17 +0530

    fix deployment

--- src/components/ManualInput.jsx
+++ src/components/ManualInput.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
-const API_URL = 'import.meta.env.VITE_API_URL';
+const API_URL = 'http://127.0.0.1:5000';
 
 const ManualInput = () => {
   const [value, setValue] = useState('');
